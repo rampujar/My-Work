4aa854f4ec8b4d2e4d2d6576be003e75d36d9f8b
rampujar <86347069+rampujar@users.noreply.github.com>
2021-07-20 10:56:20 +0530

    Update DriverCode.java

--- Data Structures/Types of DS/Custom Lined List/DriverCode.java
+++ Data Structures/Types of DS/Custom Lined List/DriverCode.java
@@ -1,1 +1,18 @@
 
+public class DriverCode {
+	public static void main(String[] args) {
+		MyList<String> m = new MyList<>();
+		m.add("a");
+		m.add("b");
+		m.add("c");
+		m.add("d");
+		m.add("e");
+		m.add("a");
+		m.add("b");
+		m.add("c");
+		System.out.println(m);
+		System.out.println(m.get(2));
+    m.remove(2)
+    System.out.println(m);
+	}
+}
